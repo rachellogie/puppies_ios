eb86a1c04357edd7ca667880a64e8a41cc6b131f
rachellogie <rachel.logie1@gmail.com>
2014-08-18 09:27:43 -0600

    Initial Commit

---
+++ Simple Puppies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Puppies
+//
+//  Created by Rachel Logie on 8/18/14.
+//  Copyright (c) 2014 Rachel Logie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
